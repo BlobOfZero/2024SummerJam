d7d4428eba53a47feedbd6a157d24759a3ff5b13
slapstickers <maxgeiringer@knights.ucf.edu>
2024-06-22 11:19:24 -0400

    Ice Enemy Functionality Done except Player Health

--- Assets/_Scripts/IceEnemyShoot.cs
+++ Assets/_Scripts/IceEnemyShoot.cs
@@ -7,23 +7,33 @@ public class IceEnemyShoot : MonoBehaviour
     public GameObject iceProj;
     public Transform projPos;
     public float fireRate;
+    public float shootDistance;
+
+
     private float timer;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer = Time.deltaTime;
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if ( timer > fireRate ) 
+        if (distance < shootDistance)
         {
-            timer = 0;
-            ShootProj();
+            timer += Time.deltaTime;
+            if (timer > fireRate)
+            {
+                ShootProj();
+                timer = 0;
+            }
         }
+
+
     }
     void ShootProj()
     {
